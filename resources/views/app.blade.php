7903447811c799b83d5eb3d4fb2db67a3064030c
LOSEARDES77 <loseardes77@gmail.com>
2025-03-03 10:15:43 +0100

    Change page title and icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Gestion de taller</title>
 
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
