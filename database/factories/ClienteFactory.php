8091c4c09b6bdc12d525a54b8983ff9887c2c6bf
LOSEARDES77 <loseardes77@gmail.com>
2025-03-07 13:30:22 +0100

    Update ClienteFactory to generate random phone numbers and rename project in package-lock.json

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -14,7 +14,7 @@ class ClienteFactory extends Factory
         return [
             'nombre' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'telefono' => $this->faker->phoneNumber(),
+            'telefono' => $this->generateNumber(),
             'dni' => $this->gen_dni($this->faker->unique()->numberBetween(0, 99999999)),
         ];
     }
@@ -25,4 +25,14 @@ class ClienteFactory extends Factory
         $dni = $num . substr($dni_letters, $num % 23, 1);
         return $dni;
     }
+
+    private function generateNumber(){
+        $number = '';
+        for ($i = 0; $i < 9; $i++) {
+            $number .= mt_rand(0, 9);
+        }
+        return $number;
+        
+    }
+
 }
